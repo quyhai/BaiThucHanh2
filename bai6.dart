c34e3af4e65718f0213c416db7ac7e52198d765c
quyhai <117881805+quyhai@users.noreply.github.com>
2023-02-16 18:09:07 +0700

    Add files via upload

---
+++ bai6.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  print("bang nhan 5 la: ");
+
+  for (int i = 1; i < 11; i++) {
+    int s = 5 * i;
+    print("5 * $i = $s");
+  }
+}
---
+++ bai7.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (int i = 1; i < 10; i++) {
+    print("bang nhan $i la ");
+    for (int j = 1; j < 11; j++) {
+      int s = i * j;
+      print("$i * $j = $s");
+    }
+  }
+}
